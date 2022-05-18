8c96acb46f856a2607c9879f7cbb258f13bf9a85
yapicimurat <muratyapiicii@gmail.com>
2022-05-18 17:29:08 +0300

    Form design and creating api folder...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import './App.css';
+import { useState } from "react";
+
+
 
 function App() {
+  const [isLogged, setIsLogged] = useState(false);
+
+
+
   return (
-    <div>
-      App component
-    </div>
+    <>
+      <h1>React and Express JWT Example...</h1>
+      <form>
+        <p>Login</p>
+        <hr></hr>
+        <label>Username</label>
+        <input type="text" id="username" name="username" />
+
+        <label>Password</label>
+        <input type="password" id="password" name="password" />
+        <button type="submit">Login</button>
+        {
+          (isLogged) ? (
+            <button type="button">Authorization</button>
+          ) : null
+        }
+      </form>
+
+
+
+    </>
   );
 }
 
