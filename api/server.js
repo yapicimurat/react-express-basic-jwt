a0154a2c35fba4c7663ecbbe3d4b2cd5bb982a27
yapicimurat <muratyapiicii@gmail.com>
2022-05-19 09:26:36 +0300

    Basic Authorization...

--- api/server.js
+++ api/server.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
-
+const jwt = require("jsonwebtoken");
 app.set('port', process.env.PORT || 3005);
 
 //APP MIDDLEWARE FOR BODY PARSER...
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cors({
+    allowedHeaders: "*",
+    methods: "*"
+}));
 
 app.get('/hello-world', (req, res) => {
     res.json({
@@ -14,4 +19,89 @@ app.get('/hello-world', (req, res) => {
     });
 });
 
-app.listen(app.get('port'), () => { console.log(`API is ready to listen on port ${app.get('port')}`);})
+
+const mockUser = {
+    id: 1,
+    username: "muratyapici",
+    password: "kalem123"
+};
+
+
+const verifyToken = (req, res, next) => {
+    const bearerHeader = req.headers['authorization'];
+
+    if (bearerHeader) {
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        //Response Forbidden...
+        res.sendStatus(403);
+    }
+};
+
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    if (username && password) {
+        if (username === mockUser.username, password === mockUser.password) {
+
+            jwt.sign({ mockUser }, 'secretKey',
+                {
+                    algorithm: "HS256",
+                    expiresIn: "1m" //1 minute-> 60.000 milliseconds
+                }, (err, token) => {
+                    if (err) {
+                        res.sendStatus(403);
+                    } else {
+                        res.json({
+                            accessToken: token
+                        });
+                    }
+                });
+
+        } else {
+            res.json({
+                error: false,
+                message: "Username or password is not correct..."
+            });
+        }
+    } else {
+        res.json(
+            {
+                error: true,
+                message: "Some values are missing...",
+            }
+        )
+    }
+
+});
+
+app.get('/getProducts', verifyToken, (req, res) => {
+    jwt.verify(req.token, 'secretKey', (err, authData) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            res.json({
+                result: [
+                    {
+                        id: 1,
+                        name: "product-1",
+                        price: 1000,
+                        moneyType: "₺"
+                    },
+                    {
+                        id: 2,
+                        name: "product-2",
+                        price: 540,
+                        moneyType: "$"
+                    }
+                ],
+                authData: authData
+            });
+        }
+    });
+});
+
+app.listen(app.get('port'), () => { console.log(`API is ready to listen on port ${app.get('port')}`); })
--- src/App.js
+++ src/App.js
@@ -1,32 +1,92 @@
 import { useState } from "react";
-
-
+import axios from "axios";
 
 function App() {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [isLogged, setIsLogged] = useState(false);
 
+  const [isFetcing, setFetching] = useState(false);
+
+
+  const submit = (e) => {
+    e.preventDefault();
+    if (username === "" || password === "") {
+      alert("Fill the blank fields...");
+    } else {
+      //firstly get access token
+
+      axios.post("http://localhost:3005/login", {
+        username: username,
+        password: password
+      })
+        .then(res => {
+
+          if (res.data.accessToken) {
+            localStorage.setItem("accessToken", res.data.accessToken);
+            setIsLogged(true);
+          } else {
+            alert(res.data.message);
+          }
+
+        })
+        .catch(err => {
+          console.error(err);
+        });
+
+
+    }
+
+  };
+
+  const authorization = () => {
+    axios.get("http://localhost:3005/getProducts",
+    {
+      headers:{
+        "Authorization": `Bearer ${localStorage.getItem('accessToken')}`
+      }
+    })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+  const valueHandler = (e) => {
+    if (e.target.name === "username") {
+      setUsername(e.target.value);
+    }
+    else if (e.target.name === "password") {
+      setPassword(e.target.value);
+    }
+  }
+
+  const saveAccessToken = (accessToken) => {
+    localStorage.setItem('accessToken', accessToken);
+  };
 
 
   return (
     <>
       <h1>React and Express JWT Example...</h1>
-      <form>
+      <form onSubmit={submit}>
         <p>Login</p>
         <hr></hr>
-        <label>Username</label>
-        <input type="text" id="username" name="username" />
+        <label title="You can see the username from the back-end side...">Username</label>
+        <input type="text" id="username" name="username" value={username} onChange={valueHandler} />
 
-        <label>Password</label>
-        <input type="password" id="password" name="password" />
-        <button type="submit">Login</button>
+        <label title="You can see the password from the back-end side...">Password</label>
+        <input type="password" id="password" name="password" value={password} onChange={valueHandler} />
+        <button type="submit">{(!isFetcing) ? "Login" : "Fetching..."}</button>
         {
           (isLogged) ? (
-            <button type="button">Authorization</button>
+            <button type="button" onClick={authorization}>Authorization</button>
           ) : null
         }
       </form>
-
-
 
     </>
   );
